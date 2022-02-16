237cbd88423ef227e6f68958e7c533f621308003
mihaelakrstv <mihaelaxkr@abv.bg>
2022-02-16 16:53:43 +0200

    sizeof

---
+++ intro/sizeof.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+int a; double b; char c;
+printf("size of a in memory is:%zu\n", sizeof(a));
+printf("size of b in memory is:%zu\n", sizeof(b));
+printf("size of c in memory is:%zu\n", sizeof(c));
+return 0;
+}
